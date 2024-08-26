f6019c1262c42bcbc9796c7df46a921240144c49
yousef_fahem <yousef.fahem11@gmail.com>
2024-08-26 07:13:36 +0300

    calculator.cpp

--- calculator.cpp
+++ calculator.cpp
@@ -5,31 +5,34 @@ using namespace std;
 int main() {
     char op;
     float num1, num2;
-
-    cout << "Enter operator: +, -, *, /: ";
+    
+    cout << "Enter the first number: ";
+    cin >> num1;
+    
+    cout << "Enter operator (+, -, *, /): ";
     cin >> op;
-
-    cout << "Enter two operands: ";
-    cin >> num1 >> num2;
+    
+    cout << "Enter the second number: ";
+    cin >> num2;
 
     switch(op) {
         case '+':
-            cout << num1 << " + " << num2 << " = " << num1 + num2;
+            cout << num1 << " + " << num2 << " = " << num1 + num2 << endl;
             break;
         case '-':
-            cout << num1 << " - " << num2 << " = " << num1 - num2;
+            cout << num1 << " - " << num2 << " = " << num1 - num2 << endl;
             break;
         case '*':
-            cout << num1 << " * " << num2 << " = " << num1 * num2;
+            cout << num1 << " * " << num2 << " = " << num1 * num2 << endl;
             break;
         case '/':
             if (num2 != 0)
-                cout << num1 << " / " << num2 << " = " << num1 / num2;
+                cout << num1 << " / " << num2 << " = " << num1 / num2 << endl;
             else
-                cout << "Division by zero is not allowed.";
+                cout << "Division by zero is not allowed." << endl;
             break;
         default:
-            cout << "Invalid operator";
+            cout << "Error: Operator not recognized. << endl;
             break;
     }
 
